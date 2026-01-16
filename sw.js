3de0e9607213f2fde6661c6a0042f392edebe092
Mazzaratti <dbray6714@yahoo.com>
2026-01-16 15:09:51 -0600

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,16 @@
+const CACHE = "booking-kit-v1";
+const ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./sw.js"];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((r) => r || fetch(e.request).then((res) => {
+      const copy = res.clone();
+      caches.open(CACHE).then((c) => c.put(e.request, copy)).catch(()=>{});
+      return res;
+    }).catch(()=>r))
+  );
+});
